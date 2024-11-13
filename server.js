db1e05b060e87b6dfbbca2124083a6c0c579cbf8
sbkrish <ranimaniyan@gmail.com>
2024-11-13 18:02:21 +0530

    https serving

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import https from 'https';
-import http from 'http';  // Import http for local server
 import express from "express";
 import logger from "./middleware/logger.js";
 import path from "path";
@@ -16,18 +15,16 @@ import registrationLimiter, { newsletterLimiter } from "./middleware/rateLimit.j
 const port = process.env.PORT || 8000;
 
 const app = express();
-app.use(cors());
-
+app.use(cors())
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Ensure you're logging the port
-console.log(`Server running on port: ${process.env.PORT || port}`);
+console.log(process.env.PORT);
 
 // Sync all models with the database
 await sequelize.sync();
 
-// Body parser for POST requests
+//body parser for post request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -36,25 +33,10 @@ app.use(logger);
 app.use("/api/students", registrationLimiter, students);
 app.use("/api/newsletter", newsletterLimiter, newsletter);
 
-// Display error for incorrect non-existing URLs
+//display error for incorrect non existing url
 app.use(notFound);
+//error middleware
 
-// Error middleware
 app.use(errorHandler);
 
-// Check if the environment is production to use HTTPS
-if (process.env.NODE_ENV === 'production') {
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.neechal.com/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/api.neechal.com/fullchain.pem', 'utf8');
-    
-    const credentials = { key: privateKey, cert: certificate };
-
-    https.createServer(credentials, app).listen(port, () => {
-        console.log(`HTTPS server running on https://localhost:${port}`);
-    });
-} else {
-    // If not production, run HTTP server for local development
-    http.createServer(app).listen(port, () => {
-        console.log(`HTTP server running on http://localhost:${port}`);
-    });
-}
+app.listen(port, () => console.log(`working server on port ${port}`));
