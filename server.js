d72923dcafc0262e6837ee1f120db7a799e86ce9
sbkrish <ranimaniyan@gmail.com>
2024-11-13 17:20:48 +0530

    https serving

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import https from 'https';
+import http from 'http';  // Import http for local server
 import express from "express";
 import logger from "./middleware/logger.js";
 import path from "path";
@@ -13,16 +16,18 @@ import registrationLimiter, { newsletterLimiter } from "./middleware/rateLimit.j
 const port = process.env.PORT || 8000;
 
 const app = express();
-app.use(cors())
+app.use(cors());
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(process.env.PORT);
+// Ensure you're logging the port
+console.log(`Server running on port: ${process.env.PORT || port}`);
 
 // Sync all models with the database
 await sequelize.sync();
 
-//body parser for post request
+// Body parser for POST requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -31,10 +36,25 @@ app.use(logger);
 app.use("/api/students", registrationLimiter, students);
 app.use("/api/newsletter", newsletterLimiter, newsletter);
 
-//display error for incorrect non existing url
+// Display error for incorrect non-existing URLs
 app.use(notFound);
-//error middleware
 
+// Error middleware
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`working server on port ${port}`));
+// Check if the environment is production to use HTTPS
+if (process.env.NODE_ENV === 'production') {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.neechal.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/api.neechal.com/fullchain.pem', 'utf8');
+    
+    const credentials = { key: privateKey, cert: certificate };
+
+    https.createServer(credentials, app).listen(port, () => {
+        console.log(`HTTPS server running on https://localhost:${port}`);
+    });
+} else {
+    // If not production, run HTTP server for local development
+    http.createServer(app).listen(port, () => {
+        console.log(`HTTP server running on http://localhost:${port}`);
+    });
+}
